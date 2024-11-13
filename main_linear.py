47983c93088d6bd01773eb5c06da3443d83ef6f5
Márcus Vinícius <usmarc.v@gmail.com>
2024-11-13 10:48:06 -0300

    feat: adicionar script principal para treinamento e validação de modelo com SupConResNet

---
+++ main_linear.py
@@ -0,0 +1,354 @@
+from __future__ import print_function
+
+import sys
+import argparse
+import time
+import math
+import os
+from pathlib import Path
+
+import torch
+import torch.backends.cudnn as cudnn
+
+from main_ce import set_loader
+from util import AverageMeter
+from util import adjust_learning_rate, warmup_learning_rate, accuracy
+from util import save_model, set_optimizer
+from networks.resnet_big import SupConResNet, LinearClassifier
+
+try:
+    import apex
+    from apex import amp, optimizers
+except ImportError:
+    pass
+
+
+def parse_option():
+    parser = argparse.ArgumentParser('argument for training')
+
+    parser.add_argument('--print_freq', type=int, default=10,
+                        help='print frequency')
+    parser.add_argument('--save_freq', type=int, default=50,
+                        help='save frequency')
+    parser.add_argument('--batch_size', type=int, default=256,
+                        help='batch_size')
+    parser.add_argument('--num_workers', type=int, default=16,
+                        help='num of workers to use')
+    parser.add_argument('--epochs', type=int, default=100,
+                        help='number of training epochs')
+
+    # optimization
+    parser.add_argument('--learning_rate', type=float, default=0.1,
+                        help='learning rate')
+    
+    parser.add_argument('--lr_decay_epochs', type=str, default='60,75,90',
+                        help='where to decay lr, can be a list')
+    
+    parser.add_argument('--lr_decay_rate', type=float, default=0.2,
+                        help='decay rate for learning rate')
+    
+    parser.add_argument('--weight_decay', type=float, default=0,
+                        help='weight decay')
+    
+    parser.add_argument('--momentum', type=float, default=0.9,
+                        help='momentum')
+
+    # model dataset
+    # Aqui tem as alterações para o modelo e dataset // mexer aqui
+    parser.add_argument('--model', type=str, default='resnet50')
+    parser.add_argument('--n_cls', type=int, default=None, help='number of classes')
+    parser.add_argument('--dataset', type=str, default='cifar10',
+                        choices=['cifar10', 'cifar100', 'imagenet100', 'imagenet', 'cifar2',
+                                 'aircraft', 'cars', 'food101', 'pet', 'dtd', 'flowers', 'path'],
+                        help='dataset')
+    parser.add_argument('--valid_split', type=float, default=0,
+                        help="proportion of train data to use for validation set")
+    parser.add_argument('--size', type=int, default=32,
+                        help='size of images after resizing')
+
+    # other setting
+    parser.add_argument('--cosine', action='store_true',
+                        help='using cosine annealing')
+    parser.add_argument('--warm', action='store_true',
+                        help='warm-up for large batch training')
+
+    parser.add_argument('--ckpt', type=str, default='',
+                        help='path to pre-trained model')
+
+    opt = parser.parse_args()
+
+
+
+    # check if dataset is path that passed required arguments
+    if opt.dataset == 'path':
+        assert opt.data_folder is not None
+        assert opt.mean is not None
+        assert opt.std is not None
+        assert opt.n_cls is not None
+
+    # set the path according to the environment
+    if opt.dataset == 'imagenet100':
+        opt.data_folder = '/cluster/tufts/hugheslab/datasets/ImageNet100/train/'
+    elif opt.dataset == 'imagenet':
+        opt.data_folder = '/cluster/tufts/hugheslab/datasets/ImageNet/train/'
+    else:
+        opt.data_folder = './datasets/'
+
+    iterations = opt.lr_decay_epochs.split(',')
+    opt.lr_decay_epochs = list([])
+    for it in iterations:
+        opt.lr_decay_epochs.append(int(it))
+
+    # get the method used by the checkpoint by grabbing everything before first _ in folder name
+    ckpt_method = Path(opt.ckpt).parts[-2].partition("_")[0]
+    opt.model_name = '{}_lr_{}_bsz_{}_{}'.\
+        format(opt.dataset, opt.learning_rate, opt.batch_size, ckpt_method)
+
+    if opt.cosine:
+        opt.model_name = '{}_cosine'.format(opt.model_name)
+
+    # warm-up for large-batch training,
+    if opt.warm:
+        opt.model_name = '{}_warm'.format(opt.model_name)
+        opt.warmup_from = 0.01
+        opt.warm_epochs = 10
+        if opt.cosine:
+            eta_min = opt.learning_rate * (opt.lr_decay_rate ** 3)
+            opt.warmup_to = eta_min + (opt.learning_rate - eta_min) * (
+                    1 + math.cos(math.pi * opt.warm_epochs / opt.epochs)) / 2
+        else:
+            opt.warmup_to = opt.learning_rate
+
+    if opt.dataset == 'cifar10':
+        opt.n_cls = 10
+    elif opt.dataset == 'cifar100':
+        opt.n_cls = 100
+    elif opt.dataset == 'cifar2':
+        opt.n_cls = 2
+    elif opt.dataset == 'imagenet100':
+        opt.n_cls = 100
+    elif opt.dataset == 'imagenet':
+        opt.n_cls = 1000
+    elif opt.dataset == 'aircraft':
+        opt.n_cls = 102
+    elif opt.dataset == 'cars':
+        opt.n_cls = 196
+    elif opt.dataset == 'food101':
+        opt.n_cls = 101
+    elif opt.dataset == 'pet':
+        opt.n_cls = 37
+    elif opt.dataset == 'dtd':
+        opt.n_cls = 47
+    elif opt.dataset == 'flowers':
+        opt.n_cls = 102
+    else:
+        raise ValueError('dataset not supported: {}'.format(opt.dataset))
+
+    opt.model_path = './save/linear/{}_models'.format(opt.dataset)
+    opt.save_folder = os.path.join(opt.model_path, opt.model_name)
+    os.makedirs(opt.save_folder, exist_ok=True)
+
+    print(opt)
+    return opt
+
+
+def set_model(opt):
+    model = SupConResNet(name=opt.model)
+    criterion = torch.nn.CrossEntropyLoss()
+
+    classifier = LinearClassifier(name=opt.model, num_classes=opt.n_cls)
+
+    ckpt = torch.load(opt.ckpt, map_location='cpu')
+    state_dict = ckpt['model']
+
+    if torch.cuda.is_available():
+        if torch.cuda.device_count() > 1:
+            model.encoder = torch.nn.DataParallel(model.encoder)
+        else:
+            new_state_dict = {}
+            for k, v in state_dict.items():
+                k = k.replace("module.", "")
+                new_state_dict[k] = v
+            state_dict = new_state_dict
+        model = model.cuda()
+        classifier = classifier.cuda()
+        criterion = criterion.cuda()
+        cudnn.benchmark = True
+
+        model.load_state_dict(state_dict)
+
+    return model, classifier, criterion
+
+
+def train(train_loader, model, classifier, criterion, optimizer, epoch, opt):
+    """one epoch training"""
+    model.eval()
+    classifier.train()
+
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+
+    end = time.time()
+    for idx, (images, labels) in enumerate(train_loader):
+        data_time.update(time.time() - end)
+
+        images = images.cuda(non_blocking=True)
+        labels = labels.cuda(non_blocking=True)
+        bsz = labels.shape[0]
+
+        # warm-up learning rate
+        warmup_learning_rate(opt, epoch, idx, len(train_loader), optimizer)
+
+        # compute loss
+        with torch.no_grad():
+            features = model.encoder(images)
+        output = classifier(features.detach())
+        loss = criterion(output, labels)
+
+        # update metric
+        losses.update(loss.item(), bsz)
+        acc1 = accuracy(output, labels, topk=(1,))[0]
+        top1.update(acc1[0], bsz)
+
+        # SGD
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        # print info
+        if (idx + 1) % opt.print_freq == 0:
+            print('Train: [{0}][{1}/{2}]\t'
+                  'BT {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'DT {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'loss {loss.val:.3f} ({loss.avg:.3f})\t'
+                  'Acc@1 {top1.val:.3f} ({top1.avg:.3f})'.format(
+                   epoch, idx + 1, len(train_loader), batch_time=batch_time,
+                   data_time=data_time, loss=losses, top1=top1))
+            sys.stdout.flush()
+
+    return losses.avg, top1.avg
+
+
+def validate(val_loader, model, classifier, criterion, opt):
+    """validation"""
+    model.eval()
+    classifier.eval()
+
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    with torch.no_grad():
+        end = time.time()
+        for idx, (images, labels) in enumerate(val_loader):
+            images = images.float().cuda()
+            labels = labels.cuda()
+            bsz = labels.shape[0]
+
+            # forward
+            output = classifier(model.encoder(images))
+            loss = criterion(output, labels)
+
+            # update metric
+            losses.update(loss.item(), bsz)
+            if opt.n_cls > 4:
+                acc1, acc5 = accuracy(output, labels, topk=(1, 5))
+                top5.update(acc5[0], bsz)
+            else:
+                acc1 = accuracy(output, labels, topk=(1,))[0]
+            top1.update(acc1[0], bsz)
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            if idx % opt.print_freq == 0:
+                print('Test: [{0}/{1}]\t'
+                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                      'Acc@1 {top1.val:.3f} ({top1.avg:.3f})'.format(
+                       idx, len(val_loader), batch_time=batch_time,
+                       loss=losses, top1=top1))
+
+    print(' * Acc@1 {top1.avg:.3f} | Acc@5 {top5.avg:.3f}'.format(top1=top1, top5=top5))
+    return losses.avg, top1.avg
+
+
+def cache_outputs(val_loader, model, classifier, opt):
+    # save model outputs for analysis and bootstrapping
+    model.eval()
+    classifier.eval()
+    # caches for outputs
+    embeds = torch.empty((0, 2048))
+    preds = torch.empty((0, opt.n_cls))
+    labels = torch.empty((0,))
+    with torch.no_grad():
+        for b_images, b_labels in val_loader:
+            b_images = b_images.float().cuda()
+            b_labels = b_labels.cuda()
+            # forward
+            b_embeds = model.encoder(b_images)
+            b_preds = classifier(b_embeds)
+            # cache
+            embeds = torch.vstack((embeds, b_embeds.cpu()))
+            preds = torch.vstack((preds, b_preds.cpu()))
+            labels = torch.hstack((labels, b_labels.cpu()))
+    # save caches
+    torch.save(embeds, os.path.join(opt.save_folder, "embeds.pth"))
+    torch.save(preds, os.path.join(opt.save_folder, "preds.pth"))
+    torch.save(labels, os.path.join(opt.save_folder, "labels.pth"))
+    return
+
+
+def main():
+    best_acc = 0
+    opt = parse_option()
+
+    # build data loader
+    train_loader, val_loader, test_loader = set_loader(opt, contrast_trans=False)
+
+    # build model and criterion
+    model, classifier, criterion = set_model(opt)
+
+    # build optimizer
+    optimizer = set_optimizer(opt, classifier)
+
+    # training routine
+    for epoch in range(1, opt.epochs + 1):
+        adjust_learning_rate(opt, optimizer, epoch)
+
+        # train for one epoch
+        time1 = time.time()
+        loss, acc = train(train_loader, model, classifier, criterion,
+                          optimizer, epoch, opt)
+        time2 = time.time()
+        print('Train epoch {}, total time {:.2f}, accuracy:{:.2f}'.format(
+            epoch, time2 - time1, acc))
+
+        # eval for one epoch
+        if val_loader is not None:
+            loss, val_acc = validate(val_loader, model, classifier, criterion, opt)
+            if val_acc > best_acc:
+                best_acc = val_acc
+        # print final accuracy for the test set evaluation run
+        elif epoch == opt.epochs:
+            validate(test_loader, model, classifier, criterion, opt)
+
+    print('best accuracy: {:.2f}'.format(best_acc))
+
+    # save the last model
+    save_file = os.path.join(
+        opt.save_folder, 'last.pth')
+    save_model(model, optimizer, opt, opt.epochs, save_file)
+    cache_outputs(test_loader, model, classifier, opt)
+
+
+if __name__ == '__main__':
+    main()
