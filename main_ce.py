31e8c0fdd6ebae6c2f9c042fc676e3e5dfa3c661
Márcus Vinícius <usmarc.v@gmail.com>
2024-12-25 10:19:05 -0400

    refactor: adicionar mensagens de depuração no loop de treino no módulo main_ce.py

--- main_ce.py
+++ main_ce.py
@@ -422,7 +422,9 @@ def train(train_loader, model, criterion, optimizer, epoch, opt):
     for idx, (images, labels) in enumerate(train_loader):
         data_time.update(time.time() - end)
 
+        print('Loop treino\n')
         images = images.cuda(non_blocking=True)
+        print('type imagens: 'type(images))
         labels = labels.cuda(non_blocking=True)
         
         bsz = labels.shape[0]
