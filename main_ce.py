10446ce1d1e86f0e8e03753f5a020397a1e54e87
Márcus Vinícius <usmarc.v@gmail.com>
2025-02-25 16:14:10 -0300

    feat: atualizar opções de modelo e remover código comentado em main_supcon.py

--- main_ce.py
+++ main_ce.py
@@ -15,7 +15,7 @@ from torch.utils.data import DataLoader, DistributedSampler, random_split
 from torchvision import transforms, datasets
 
 import sampler
-from util import AverageMeter, DoubleTransform, SubsetWithTargets, TwoCropTransform
+from util import AverageMeter, DoubleTransform, SubsetWithTargets, TwoCropTransform, CustomDatasetFromCSV
 from util import adjust_learning_rate, warmup_learning_rate, accuracy, set_optimizer, save_model
 
 
@@ -56,6 +56,7 @@ def parse_option():
     parser.add_argument('--dataset', type=str, default='cifar10',
                         choices=['cifar10', 'cifar100', 'imagenet100', 'imagenet', 'path'],
                         help='dataset')
+
     parser.add_argument('--valid_split', type=float, default=0,
                         help="proportion of train data to use for validation set")
     parser.add_argument('--mean', type=str,
@@ -85,7 +86,7 @@ def parse_option():
 
     # check if dataset is path that passed required arguments
     if opt.dataset == 'path':
-        opt.data_folder is not None
+        #opt.data_folder is not None
         opt.mean is not None
         opt.std is not None
         opt.n_cls is not None
@@ -152,247 +153,88 @@ def parse_option():
 
 
 def set_loader(opt, contrast_trans=False, for_test=False):
-
-    # dataset specific normalization
+    
     if opt.dataset == 'cifar10':
         mean = (0.4914, 0.4822, 0.4465)
         std = (0.2023, 0.1994, 0.2010)
     elif opt.dataset == 'cifar100':
         mean = (0.5071, 0.4867, 0.4408)
         std = (0.2675, 0.2565, 0.2761)
-    elif opt.dataset == 'cifar2':
-        mean = (0.4977, 0.4605, 0.4160)
-        std = (0.2537, 0.2481, 0.2535)
-    elif opt.dataset == 'aircraft':
-        mean = (0.4804, 0.5115, 0.5348)
-        std = (0.1561, 0.1555, 0.1810)
-    elif opt.dataset == 'cars':
-        mean = (0.4707, 0.4601, 0.4549)
-        std = (0.2319, 0.2318, 0.2373)
-    elif opt.dataset == 'food101':
-        mean = (0.5456, 0.4430, 0.3423)
-        std = (0.2096, 0.2186, 0.2165)
-    elif opt.dataset == 'pet':
-        mean = (0.4786, 0.4462, 0.3962)
-        std = (0.2073, 0.2042, 0.2053)
-    elif opt.dataset == 'dtd':
-        mean = (0.5301, 0.4734, 0.4243)
-        std = (0.1250, 0.1246, 0.1211)
-    elif opt.dataset == 'flowers':
-        mean = (0.4312, 0.3786, 0.2944)
-        std = (0.2385, 0.1858, 0.1986)
-    elif opt.dataset == 'imagenet100' or opt.dataset == 'imagenet':
-        mean = (0.485, 0.456, 0.406)
-        std = (0.229, 0.224, 0.225)
     elif opt.dataset == 'path':
         mean = eval(opt.mean)
         std = eval(opt.std)
     else:
         raise ValueError('dataset not supported: {}'.format(opt.dataset))
+    
     normalize = transforms.Normalize(mean=mean, std=std)
 
-    # contrastive data transforms
     if contrast_trans:
-        # first image is non-augmented, second is lightly augmented
-        test_transform = DoubleTransform(
-            transforms.Compose([
-                transforms.Resize([opt.size, opt.size]),
-                transforms.ToTensor(),
-                normalize,
-            ]),
-            transforms.Compose([
-                transforms.RandomResizedCrop(size=opt.size, scale=(0.2, 1.)),
-                transforms.RandomHorizontalFlip(),
-                transforms.ToTensor(),
-                normalize,
-            ]))
-        if for_test:
-            train_transform = test_transform
-        else:
-            # both images heavily augmented
-            train_transform = TwoCropTransform(transforms.Compose([
-                transforms.RandomResizedCrop(size=opt.size, scale=(0.2, 1.)),
-                transforms.RandomHorizontalFlip(),
-                transforms.RandomApply([
-                    transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)
-                ], p=0.8),
-                transforms.RandomGrayscale(p=0.2),
-                transforms.ToTensor(),
-                normalize,
-            ]))
-    # non-contrastive data transforms == False
-    else:
+        #both images heavily augmented based SupCon repo: https://github.com/HobbitLong/SupContrast/blob/master/main_supcon.py#L146
+        train_transform = TwoCropTransform(transforms.Compose([
+                            transforms.RandomResizedCrop(size=opt.size, scale=(0.2, 1.)),
+                            transforms.RandomHorizontalFlip(),
+                            transforms.RandomApply([
+                                transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)
+                            ], p=0.8),
+                            transforms.RandomGrayscale(p=0.2),
+                            transforms.ToTensor(),
+                            normalize,
+        ]))
+        if for_test: #Validation Split
+            val_transform = train_transform
+    else: #Non-contrastive data transforms == False / main_ce.py or main_linear.py
         train_transform = transforms.Compose([
             transforms.RandomResizedCrop(size=opt.size, scale=(0.2, 1.)),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             normalize,
         ])
-        test_transform = transforms.Compose([
+        val_transform = transforms.Compose([
             transforms.Resize([opt.size, opt.size]),
             transforms.ToTensor(),
             normalize,
-        ])
-
-    # construct dataset
-    if opt.dataset == 'cifar10' or opt.dataset == 'cifar2':
+        ]) 
+    
+    #Contrusciton of dataset
+    if opt.dataset == 'cifar10':
         train_dataset = datasets.CIFAR10(root=opt.data_folder,
                                          transform=train_transform,
                                          download=True)
-        test_dataset = datasets.CIFAR10(root=opt.data_folder,
+        val_dataset = datasets.CIFAR10(root=opt.data_folder,
                                         train=False,
-                                        transform=test_transform)
-        # produce CIFAR-2 (cats vs dogs) from CIFAR-10
-        if opt.dataset == 'cifar2':
-            classes = torch.Tensor([train_dataset.class_to_idx[name] for name in ["cat", "dog"]])
-            cls_idx_map = {int(classes[0]): 0, int(classes[1]): 1}
-            train_ind = torch.where(torch.isin(torch.Tensor(train_dataset.targets), classes))[0]
-            train_dataset.target_transform = lambda x: cls_idx_map[x]
-            train_dataset = SubsetWithTargets(train_dataset, train_ind)
-            test_ind = torch.where(torch.isin(torch.Tensor(test_dataset.targets), classes))[0]
-            test_dataset.target_transform = lambda x: cls_idx_map[x]
-            test_dataset = SubsetWithTargets(test_dataset, test_ind)
+                                        transform=val_transform)
     elif opt.dataset == 'cifar100':
         train_dataset = datasets.CIFAR100(root=opt.data_folder,
                                           transform=train_transform,
                                           download=True)
-        test_dataset = datasets.CIFAR100(root=opt.data_folder,
+        val_dataset = datasets.CIFAR100(root=opt.data_folder,
                                          train=False,
-                                         transform=test_transform)
-    elif opt.dataset == 'aircraft':
-        train_dataset = datasets.FGVCAircraft(root=opt.data_folder,
-                                              split="trainval",
-                                              transform=train_transform,
-                                              download=True)
-        train_dataset.targets = train_dataset._labels
-        test_dataset = datasets.FGVCAircraft(root=opt.data_folder,
-                                             split="test",
-                                             transform=test_transform)
-        test_dataset.targets = test_dataset._labels
-    elif opt.dataset == 'cars':
-        train_dataset = datasets.StanfordCars(root=opt.data_folder,
-                                              transform=train_transform)
-        train_dataset.targets = [label for _, label in train_dataset._samples]
-        test_dataset = datasets.StanfordCars(root=opt.data_folder,
-                                             split="test",
-                                             transform=test_transform)
-        test_dataset.targets = [label for _, label in test_dataset._samples]
-    elif opt.dataset == 'food101':
-        train_dataset = datasets.Food101(root=opt.data_folder,
-                                         transform=train_transform,
-                                         download=True)
-        train_dataset.targets = train_dataset._labels
-        test_dataset = datasets.Food101(root=opt.data_folder,
-                                        split="test",
-                                        transform=test_transform,
-                                        download=True)
-        test_dataset.targets = test_dataset._labels
-    elif opt.dataset == 'pet':
-        train_dataset = datasets.OxfordIIITPet(root=opt.data_folder,
-                                               transform=train_transform,
-                                               download=True)
-        train_dataset.targets = train_dataset._labels
-        test_dataset = datasets.OxfordIIITPet(root=opt.data_folder,
-                                              split="test",
-                                              transform=test_transform,
-                                              download=True)
-        test_dataset.targets = test_dataset._labels
-    elif opt.dataset == 'dtd':
-        train_dataset = datasets.DTD(root=opt.data_folder,
-                                     transform=train_transform,
-                                     download=True)
-        train_dataset.targets = train_dataset._labels
-        test_dataset = datasets.DTD(root=opt.data_folder,
-                                    split="test",
-                                    transform=test_transform,
-                                    download=True)
-        test_dataset.targets = test_dataset._labels
-    elif opt.dataset == 'flowers':
-        train_dataset = datasets.Flowers102(root=opt.data_folder,
-                                            transform=train_transform,
-                                            download=True)
-        train_dataset.targets = train_dataset._labels
-        test_dataset = datasets.Flowers102(root=opt.data_folder,
-                                           split="test",
-                                           transform=test_transform,
-                                           download=True)
-        test_dataset.targets = test_dataset._labels
-
-    elif opt.dataset == 'imagenet100' or opt.dataset == 'imagenet' or opt.dataset == 'path':
-        if opt.percentDataset:
-            train_dataset = datasets.ImageFolder(root=opt.data_folder + "/train/",
-                                                 transform=train_transform)
-            subset = int(len(train_dataset)*opt.percentDataset)
-            _,subset = random_split(train_dataset, [len(train_dataset)-subset, subset])
-
-            test_dataset = datasets.ImageFolder(root=opt.data_folder + "/test/",
-                                                transform=test_transform)
-
-        else:
-            train_dataset = datasets.ImageFolder(root=opt.data_folder + "/train/",
-                                                transform=train_transform)
-            test_dataset = datasets.ImageFolder(root=opt.data_folder + "/test/",
-                                                transform=test_transform)
+                                         transform=val_transform)
+    elif opt.dataset == 'path':
+        # train_files = 'Datasets/KFolds/SKF_TRAIN_Fold_1.csv'
+        # val_files = 'Datasets/KFolds/SKF_VAL_Fold_1.csv'
+        train_dataset = CustomDatasetFromCSV(opt.root_path, tf_image=train_transform, csv_name=opt.train_files, task=None, as_rgb=True)
+        val_dataset = CustomDatasetFromCSV(opt.root_path, tf_image=val_transform, csv_name=opt.val_files, task=None, as_rgb=True)
     else:
-        raise ValueError(opt.dataset)
-
-    if opt.valid_split == 0:
-        valid_loader = None
-    # split validation off of train
-    else:
-        old_train = train_dataset
-        old_val = copy.deepcopy(train_dataset)
-        old_val.transform = test_transform
-        train_ind, valid_ind = train_test_split(
-            range(len(old_train)), stratify=old_train.targets, test_size=opt.valid_split,
-            random_state=12345)
-        train_dataset = SubsetWithTargets(old_train, train_ind)
-        valid_dataset = SubsetWithTargets(old_val, valid_ind)
-        # construct validation data loader
-        if "device" in opt:
-            valid_loader = DataLoader(
-                valid_dataset, num_workers=opt.num_workers, pin_memory=True,
-                batch_size=opt.batch_size,
-                sampler=DistributedSampler(valid_dataset) if "device" in opt else None)
-        else:
-            valid_loader = DataLoader(
-                valid_dataset, num_workers=opt.num_workers, pin_memory=True,
-                batch_sampler=sampler.my_sampler(valid_dataset, opt.batch_size))
-
-    # construct train and test data loaders
-    if "device" in opt:
-        train_loader = DataLoader(
-                train_dataset, num_workers=opt.num_workers, pin_memory=True,
-                batch_size=opt.batch_size,
-                sampler=DistributedSampler(train_dataset) if "device" in opt else None)
-        test_loader = DataLoader(
-                test_dataset, num_workers=opt.num_workers, pin_memory=True,
-                batch_size=opt.batch_size,
-                sampler=DistributedSampler(test_dataset) if "device" in opt else None)
-    elif not for_test:
-        train_loader = DataLoader(
-            train_dataset, num_workers=opt.num_workers, pin_memory=True,
-            batch_sampler=sampler.my_sampler(train_dataset, opt.batch_size))
-        test_loader = DataLoader(
-            test_dataset, num_workers=opt.num_workers, pin_memory=True,
-            batch_sampler=sampler.my_sampler(test_dataset, opt.batch_size))
-    else:
-        if opt.percentDataset:
-            train_loader = DataLoader(
-                subset, num_workers=opt.num_workers, pin_memory=True,
-                batch_size=opt.batch_size,
-                sampler=DistributedSampler(subset) if "device" in opt else None)
-            test_loader = DataLoader(
-                test_dataset, num_workers=opt.num_workers, pin_memory=True,
-                batch_size=opt.batch_size,
-                sampler=DistributedSampler(test_dataset) if "device" in opt else None)
-        else:
-            train_loader = DataLoader(
-                train_dataset, num_workers=opt.num_workers, pin_memory=True,
-                batch_size=opt.batch_size)
-            test_loader = DataLoader(
-                test_dataset, num_workers=opt.num_workers, pin_memory=True,
-                batch_size=opt.batch_size)
+        raise ValueError(f'Dataset', opt.dataset, 'not supported')
+    
+    # #Construct train and test data loaders
+    # if opt.dataset == 'path': #Load from folders
+    #     train_dataset = datasets.ImageFolder(root=opt.data_folder + "/train/",
+    #                                             transform=train_transform)
+    #     val_loader = datasets.ImageFolder(root=opt.data_folder + "/val/",
+    #                                             transform=val_transform)
+    # else: #Load from fcsv
+    train_loader = DataLoader(train_dataset,
+                                batch_size=opt.batch_size,
+                                num_workers=opt.num_workers,
+                                pin_memory=True,
+                                shuffle=True)
+    val_loader = DataLoader(val_dataset,
+                                batch_size=opt.batch_size,
+                                num_workers=opt.num_workers,    
+                                pin_memory=True,
+                                shuffle=False)
 
     # compute mean and STD used above (use test transform without normalization)
     # code from https://discuss.pytorch.org/t/about-normalization-using-pre-trained
@@ -411,7 +253,7 @@ def set_loader(opt, contrast_trans=False, for_test=False):
     # print(mean)
     # print(std)
 
-    return train_loader, valid_loader, test_loader
+    return train_loader, val_loader
 
 
 def set_model(opt):
--- main_supcon.py
+++ main_supcon.py
@@ -10,27 +10,19 @@ import torch
 import torch.backends.cudnn as cudnn
 from torch.utils.tensorboard import SummaryWriter
 
+#Measures and utils
 from contrast_acc import contrastive_acc, test_contrastive_acc, test_contrastive_acc_knn
 from main_ce import set_loader
 from util import AverageMeter, adjust_learning_rate, warmup_learning_rate, set_optimizer, save_model
 
 #Networks
-from networks.pretrained_models import load_pretrained_model
-# import timm
-# import networks.vit as vits #From scratch ViT models
-# from networks.dino_models import load_dino_model, download_checkpoint, MultiCropWrapper
-# from networks.resnet_big import SupConResNet #From scratch ResNet models
-# from torchvision.models import resnet50, ResNet50_Weights
+import networks.vit as vits
+from networks.resnet_big import SupConResNet
+from torchvision.models import resnet50, ResNet50_Weights
 
 #Losses
 from losses import SupConLoss, MultiviewSINCERELoss, MultiviewEpsSupInfoNCELoss, InfoNCELoss
 
-import util
-
-
-__all_models = ['resnet50', 
-                'vit_small', 'vit_base', 
-                'dino_vit_small_p_16', 'dino_vit_small_p_8', 'dino_vit_base_p_16', 'dino_vit_base_p_8']
 
 def parse_option():
 
@@ -60,10 +52,8 @@ def parse_option():
                         help='momentum')
 
     # model dataset
-    parser.add_argument('--model', type=str, default='resnet50', choices=['resnet50', 
-                                                                          'vit_small', 'vit_base',
-                                                                          'dino_vit_small_p_16', 'dino_vit_small_p_8', 
-                                                                          'dino_vit_base_p_16', 'dino_vit_base_p_8'], 
+    parser.add_argument('--model', type=str, default='resnet50', choices=['resnet18', 'resnet50', 'resnet101', 
+                                                                          'vit_tiny', 'vit_base', 'vit_small', 'vit_large'], 
                         help='Choose your backbone')
     #parser.add_argument('--n_cls', type=int, default=None, help='Number of classes for your dataset')
     parser.add_argument('--dataset', type=str, default='cifar10',
@@ -79,8 +69,14 @@ def parse_option():
                         default=None, help='path to custom dataset')
     parser.add_argument('--size', type=int, default=32,
                         help='size of images after resizing')
+    
+    # data augmentation
+    # parser.add_argument('--crop_size', type=int, default=320, help='parameter for RandomResizedCrop')
+    # parser.add_argument('--crop_scale', type=str, help='crop scale for RandomResizedCrop in form of str tuple')
+    # parser.add_argument('--crop_ratio', type=str, help='crop ratio for RandomResizedCrop in form of str tuple')
+    # parser.add_argument('--degrees', type=int, help='limit for degrees used in random rotation augmentation')
 
-    # Contrastive method
+    # method
     parser.add_argument('--method', type=str, default='SINCERE',
                         choices=['SINCERE', 'SupCon', 'EpsSupInfoNCE', 'SimCLR', 'InfoNCE'],
                         help='Choose your contrastive method')
@@ -98,12 +94,6 @@ def parse_option():
                         help='warm-up for large batch training')
     parser.add_argument('--trial', type=str, default='0',
                         help='id for recording multiple runs')
-    
-
-    #Experimental Dataset Settings
-    parser.add_argument('--kfold', type=int, default=10, help='K-fold cross validation for 30 percent for train dataset')
-    parser.add_argument('--percentDataset', type=float, default=0.3, help='Percentage of dataset to use for splint and use K-fold training')
-
 
     opt = parser.parse_args()
 
@@ -190,60 +180,22 @@ def set_model(opt):
 
     print('\n[INFO] Setting model and criterion...')
 
-
-    # DINO_MODELS = {
-    #     "dino_vit_small_p_16": "https://dl.fbaipublicfiles.com/dino/dino_deitsmall16_pretrain/dino_deitsmall16_pretrain_full_checkpoint.pth",
-    #     "dino_vit_small_p_8": "https://dl.fbaipublicfiles.com/dino/dino_deitsmall8_pretrain/dino_deitsmall8_pretrain_full_checkpoint.pth",
-    #     "dino_vit_base_p_16": "https://dl.fbaipublicfiles.com/dino/dino_vitbase16_pretrain/dino_vitbase16_pretrain_full_checkpoint.pth",
-    #     "dino_vit_base_p_8": "https://dl.fbaipublicfiles.com/dino/dino_vitbase8_pretrain/dino_vitbase8_pretrain_full_checkpoint.pth"
-    # }
-
     # Set the model
-    model = None
-
-    if opt.model in __all_models:
-        model = load_pretrained_model(opt.model)
-
-    # # ViT model
-    # if opt.model == "vit_small": 
-    #     pretrained_net = timm.create_model('vit_small_patch16_224', pretrained=True)
-    #     model = vits.SupConViT(name=opt.model, feat_dim=384)
-    #     pretrained_net.head = torch.nn.Identity()
-    #     for pretrained_net.head in pretrained_net.head.parameters():
-    #         pretrained_net.head = True
-    #     model.encoder.load_state_dict(pretrained_net.state_dict(), strict=False)
-
-    # elif opt.model == "vit_base":
-    #     pretrained_net = timm.create_model('vit_base_patch16_224', pretrained=True)
-    #     model = vits.SupConViT(name=opt.model, feat_dim=384)
-    #     pretrained_net.head = torch.nn.Identity()
-    #     for pretrained_net.head in pretrained_net.head.parameters():
-    #         pretrained_net.head = True
-    #     model.encoder.load_state_dict(pretrained_net.state_dict(), strict=False)
-    
-    # elif "resnet50" in opt.model: 
-    #     # Visit: https://github.com/HobbitLong/SupContrast/issues/146
-    #     pretrained_net = resnet50(weights=ResNet50_Weights.DEFAULT)
-    #     model = SupConResNet(name=opt.model)
-    #     pretrained_net.fc = torch.nn.Identity()
-    #     model.encoder.load_state_dict(pretrained_net.state_dict(), strict=False)
-
-    # elif "dino_vit_small_p_16" in opt.model:
-    #     pretrained_net = torch.hub.load('facebookresearch/dino:main', 'dino_vits16')
-    #     model = vits.SupConViT(name=opt.model, feat_dim=384)
-    #     pretrained_net.head = torch.nn.Identity()
-    #     for pretrained_net.head in pretrained_net.head.parameters():
-    #         pretrained_net.head = True
-    #     model.encoder.load_state_dict(pretrained_net.state_dict(), strict=False)
-
-    # # elif opt.model in DINO_MODELS:
-    # #     checkpoint_path = download_checkpoint(opt.model)    
-    # #     if checkpoint_path is None:
-    # #         raise FileNotFoundError(f"[ERROR] Não foi possível encontrar o checkpoint {opt.model}.")        
-    # #     model = load_dino_model(opt.model, checkpoint_path, opt.checkpoint_key) #isso funciona 
-    # else:
-    #     raise ValueError(f"Choose model [{opt.model}] not supported.")
-    
+    # If model is ViT
+    if "vit_tiny" in opt.model: 
+        model = vits.SupConViT(name=opt.model, feat_dim=192)
+    elif "vit_small" in opt.model: 
+        model = vits.SupConViT(name=opt.model, feat_dim=384)
+    elif "vit_base" in opt.model: 
+        model = vits.SupConViT(name=opt.model, feat_dim=768)
+    elif "vit_large" in opt.model: 
+        model = vits.SupConViT(name=opt.model, feat_dim=1024)
+    else: #If model is resnet
+        pretrained_weights = resnet50(weights=ResNet50_Weights.DEFAULT)
+        pretrained_weights.fc = torch.nn.Identity()
+        model = SupConResNet(name=opt.model)
+        model.encoder.load_state_dict(pretrained_weights.state_dict(), strict=False)
+     
     #Set criterion 
     if opt.method == 'SINCERE':
         # original implementation does not set base_temperature, but setting here to make
@@ -369,14 +321,13 @@ def valid(train_loader, valid_loader, epoch, opt, logger):
     # Define as dimensões de embeddings para diferentes arquiteturas
     # Reajustar para não chamar toda vez
     embedding_dims = {
+        'resnet18': 128,
+        'resnet101': 128,
         'resnet50': 128,
+        'vit_tiny': 192,
         'vit_small': 384,
         'vit_base': 768,
-        'vit_large': 1024,
-        "dino_vit_small_p_16": 384,
-        "dino_vit_small_p_8": 384,
-        "dino_vit_base_p_16": 768,
-        "dino_vit_base_p_8": 768
+        'vit_large': 1024
     }
     
     embedding_dim = embedding_dims.get(opt.model)  
