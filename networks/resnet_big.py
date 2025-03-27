0a792f4dcc86ff4c23d2228b9e17d405f4e4f194
Márcus Vinícius <usmarc.v@gmail.com>
2025-03-27 14:20:32 -0300

    feat: ajustar o stride e padding da primeira camada convolucional na ResNet

--- networks/resnet_big.py
+++ networks/resnet_big.py
@@ -83,7 +83,7 @@ class ResNet(nn.Module):
         super(ResNet, self).__init__()
         self.in_planes = 64
         #kernel size with 7x7 because ImageNet images are 224x224
-        self.conv1 = nn.Conv2d(in_channel, 64, kernel_size=7, stride=1, padding=1,
+        self.conv1 = nn.Conv2d(in_channel, 64, kernel_size=7, stride=2, padding=3,
                                bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
