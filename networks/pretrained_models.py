47d6642fd550272a5e2e999f13b13bfcb49bcaa3
Márcus Vinícius <usmarc.v@gmail.com>
2025-04-03 16:20:18 -0300

    feat: adicionar suporte para novos modelos dinov2 e ajustar embeddings posicionais

--- networks/pretrained_models.py
+++ networks/pretrained_models.py
@@ -1,38 +1,36 @@
-import torch
 import timm
+import torch
+import torch.nn.functional as F
 import networks.vit as vits
+import networks.dinov2 as dinov2
 from networks.resnet_big import SupConResNet
 from torchvision.models import resnet50, ResNet50_Weights
 
 
 def load_pretrained_model(model_name):
-    """Load a pre-trained model from the timm library or from the DINO repository.
+    """Load pretrained model from timm models or torch hub
 
     Args:
-        model_name (str): Model name to be loaded. Supported models are:
-            - resnet50
-            - vit_small
-            - vit_base
-            - dino_vit_small_p_16
-            - dino_vit_small_p_8
-            - dino_vit_base_p_16
-            - dino_vit_base_p_8
+        model_name (_type_): _description_
 
     Raises:
-        ValueError: Unsupported model name.
+        ValueError: _description_
+        ValueError: _description_
 
     Returns:
-        model (torch.nn.Module): Model with pre-trained weights based on the model_name.
+        _type_: _description_
     """    
 
     model_mapping = {
         "resnet50": ("resnet50", 128),
         "vit_small": ("vit_small_patch16_224", 384),
         "vit_base": ("vit_base_patch16_224", 768),
-        "dino_vit_small_p_16": ("dino_vit_small_patch16_224", 384),
-        "dino_vit_small_p_8": ("dino_vit_small_patch8_224", 384),
-        "dino_vit_base_p_16": ("dino_vit_base_patch16_224", 768),
-        "dino_vit_base_p_8": ("dino_vit_base_patch8_224", 768)
+        "dino_vit_small_p_16": ("dino_vits16", 384),
+        "dino_vit_small_p_8": ("dino_vits8", 384),
+        "dino_vit_base_p_16": ("dino_vitb16", 768),
+        "dino_vit_base_p_8": ("dino_vitb8", 768),
+        "dinov2_vit_small_p_14": ("dinov2_vits14", 384),
+        "dinov2_vit_base_p_14": ("dinov2_vitb14", 768)
     }
     
     if model_name not in model_mapping:
@@ -55,15 +53,40 @@ def load_pretrained_model(model_name):
     elif model_name == "dino_vit_small_p_16" or model_name == "dino_vit_small_p_8":
         if "p_16" in model_name:
             pretrained_net = torch.hub.load("facebookresearch/dino:main", "dino_vits16")
+            # pretrained_net = timm.create_model(all_model_name, pretrained=True)
         elif "p_8" in model_name:
             pretrained_net = torch.hub.load("facebookresearch/dino:main", "dino_vits8")
     
     elif model_name == "dino_vit_base_p_16" or model_name == "dino_vit_base_p_8":
         if "p_16" in model_name:
+            # pretrained_net = timm.create_model(all_model_name, pretrained=True)
             pretrained_net = torch.hub.load("facebookresearch/dino:main", "dino_vitb16")
         elif "p_8" in model_name:
             pretrained_net = torch.hub.load("facebookresearch/dino:main", "dino_vitb8")
+            # pretrained_net = timm.create_model(all_model_name, pretrained=True)
+    elif model_name == "dinov2_vit_small_p_14" or model_name == "dinov2_vit_base_p_14":
+        if "small" in model_name:
+            pretrained_net = torch.hub.load("facebookresearch/dinov2:main", "dinov2_vits14")
+        elif "base" in model_name:
+            pretrained_net = torch.hub.load("facebookresearch/dinov2:main", "dinov2_vitb14")
+        
+        pretrained_net.head = torch.nn.Identity()
+        model = dinov2.DinoSupConViT(name=model_name, feat_dim=feat_dim)
+        pretrained_pos_embed = pretrained_net.pos_embed  # Positional embeddings do modelo pré-treinado
+        num_tokens = model.encoder.pos_embed.shape[1]  # Número esperado de tokens
+        # Interpola para corresponder ao número esperado de tokens
+        new_pos_embed = F.interpolate(pretrained_pos_embed.unsqueeze(0), 
+                                        size=(num_tokens, pretrained_pos_embed.shape[2]), 
+                                        mode='bilinear', align_corners=False).squeeze(0)
 
+        # Substitui os embeddings posicionais no modelo carregado
+        pretrained_net.pos_embed = torch.nn.Parameter(new_pos_embed)
+        for param in pretrained_net.head.parameters():
+            param.requires_grad = True
+        model.encoder.load_state_dict(pretrained_net.state_dict(), strict=False)
+
+        return model
+    
     else:
         raise ValueError(f"Unsupported model name: {model_name}")
 
