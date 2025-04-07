1f6dc4135217b9f6e035f5370cfb0ec116826cc4
Márcus Vinícius <usmarc.v@gmail.com>
2025-04-07 09:14:50 -0300

    feat: atualizar parâmetros de avaliação e caminhos de checkpoint no script run_main_linear.sh

--- run_main_linear.sh
+++ run_main_linear.sh
@@ -2,19 +2,19 @@
 
 checkpoints=(
     
-    "save_2025_03_31/SINCERE/path_models/SINCERE_path_resnet50_lr_0.0001_decay_0.0001_bsz_128_temp_0.07_trial_0_cosine_warm_2025_03_31-20_58_46/last_fold_1.pth"
-    "save_2025_04_02/SINCERE/path_models/SINCERE_path_vit_small_lr_0.0001_decay_0.0001_bsz_128_temp_0.07_trial_0_cosine_warm_2025_04_02-11_52_52/last_fold_1.pth"
-
+    #"save_exp_subfolds/save_2025_03_31/SINCERE/path_models/SINCERE_path_resnet50_lr_0.0001_decay_0.0001_bsz_128_temp_0.07_trial_0_cosine_warm_2025_03_31-20_58_46/last_fold_1.pth"
+    #"save_2025_04_02/SINCERE/path_models/SINCERE_path_vit_small_lr_0.0001_decay_0.0001_bsz_128_temp_0.07_trial_0_cosine_warm_2025_04_02-11_52_52/last_fold_1.pth"
+    "save_exp_subfolds/save_2025_04_02/SINCERE/path_models/SINCERE_path_vit_small_lr_0.0001_decay_0.0001_bsz_128_temp_0.07_trial_0_cosine_warm_2025_04_02-11_52_52/last_fold_1.pth"
 )
 
 # Parâmetros gerais do comando
-batch_size=128
+batch_size=32
 n_cls=10
-size=224
+size=128
 learning_rate=0.0001
 dataset="path"
 num_workers=8
-# model="resnet50"
+model="vit_small"
 
 # Loop para executar a avaliação para cada checkpoint
 for ckpt in "${checkpoints[@]}"; do
@@ -26,12 +26,13 @@ for ckpt in "${checkpoints[@]}"; do
         --batch_size $batch_size \
         --n_cls $n_cls \
         --size $size \
+        --model $model \
         --dataset $dataset \
         --learning_rate $learning_rate \
         --num_workers $num_workers \
-        --epochs 100 \
+        --epochs 2 \
         --ckpt $ckpt \
-        > "${method_name}_LinearEvalStage-test100epochs-final.txt"
+        > "${method_name}_LinearEvalStage-${model}.txt"
 
     echo "Avaliação para o checkpoint $ckpt finalizada."
 done
