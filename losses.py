c805b069c2118e9e27947778082b76e4b30d2fc6
Márcus Vinícius <usmarc.v@gmail.com>
2025-05-01 10:34:15 -0300

    feat: update

--- losses.py
+++ losses.py
@@ -6,11 +6,7 @@ import torch.nn as nn
 
 class SupConLoss(nn.Module):
     """Supervised Contrastive Learning: https://arxiv.org/pdf/2004.11362.pdf.
-    It also supports the unsupervised contrastive loss in SimCLR
-
-    Author: Yonglong Tian (yonglong@mit.edu)
-    Date: May 07, 2020
-    """
+    It also supports the unsupervised contrastive loss in SimCLR"""
     def __init__(self, temperature=0.07, contrast_mode='all',
                  base_temperature=0.07):
         super(SupConLoss, self).__init__()
@@ -82,18 +78,6 @@ class SupConLoss(nn.Module):
             torch.arange(batch_size * anchor_count).view(-1, 1).to(device),
             0
         )
-        # mask = mask * logits_mask
-
-        # # compute log_prob
-        # exp_logits = torch.exp(logits) * logits_mask
-        # log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
-
-        # # compute mean of log-likelihood over positive
-        # mean_log_prob_pos = (mask * log_prob).sum(1) / mask.sum(1)
-
-        # # loss
-        # loss = - (self.temperature / self.base_temperature) * mean_log_prob_pos
-        # loss = loss.view(anchor_count, batch_size).mean()
         mask = mask * logits_mask
 
         # compute log_prob
@@ -101,21 +85,14 @@ class SupConLoss(nn.Module):
         log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
 
         # compute mean of log-likelihood over positive
-        # modified to handle edge cases when there is no positive pair
-        # for an anchor point. 
-        # Edge case e.g.:- 
-        # features of shape: [4,1,...]
-        # labels:            [0,1,1,2]
-        # loss before mean:  [nan, ..., ..., nan] 
-        mask_pos_pairs = mask.sum(1)
-        mask_pos_pairs = torch.where(mask_pos_pairs < 1e-6, 1, mask_pos_pairs)
-        mean_log_prob_pos = (mask * log_prob).sum(1) / mask_pos_pairs
+        mean_log_prob_pos = (mask * log_prob).sum(1) / mask.sum(1)
 
         # loss
         loss = - (self.temperature / self.base_temperature) * mean_log_prob_pos
         loss = loss.view(anchor_count, batch_size).mean()
 
         return loss
+
 
 class SINCERELoss(nn.Module):
     def __init__(self, temperature=0.07) -> None:
